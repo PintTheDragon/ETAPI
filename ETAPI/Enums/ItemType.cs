1a58141dd881dd6cd929971fdb12b06e6c2d86ab
PintTheDragon <PintTheDragon@hotmail.com>
2021-01-07 22:53:51 -0800

    Added items

--- ETAPI/Enums/ItemType.cs
+++ ETAPI/Enums/ItemType.cs
@@ -2,6 +2,32 @@
 {
     public enum ItemType
     {
-        None
+        None,
+        Ammo,
+        AnomGasmask,
+        Badge,
+        Battery,
+        BVGoggles,
+        Document,
+        Eyedrops,
+        Flashlight,
+        Gasmask,
+        IVGoggles,
+        Janitor1,
+        Keycard0,
+        Keycard1,
+        Keycard2,
+        Keycard3,
+        Keycard4,
+        Keycard5,
+        Medkit,
+        NVGoggles,
+        P90,
+        Radio,
+        SCP714,
+        SCP035,
+        SCP1025,
+        SCP1499,
+        SNav
     }
 }
--- ETAPI/Extensions/Items.cs
+++ ETAPI/Extensions/Items.cs
@@ -6,7 +6,33 @@ namespace ETAPI.Extensions
     {
         private static Dictionary<string, ItemType> ItemIDs = new Dictionary<string, ItemType>()
         {
-            
+            {"", ItemType.None},
+            {"Ammo", ItemType.Ammo},
+            {"AnomGasmask", ItemType.AnomGasmask},
+            {"Badge", ItemType.Badge},
+            {"Battery", ItemType.Battery},
+            {"BVGoggles", ItemType.BVGoggles},
+            {"ItemDocument", ItemType.Document},
+            {"Eyedrops", ItemType.Eyedrops},
+            {"Flashlight", ItemType.Flashlight},
+            {"Gasmask", ItemType.Gasmask},
+            {"IVGoggles", ItemType.IVGoggles},
+            {"Janitor1", ItemType.Janitor1},
+            {"Keycard0", ItemType.Keycard0},
+            {"Keycard1", ItemType.Keycard1},
+            {"Keycard2", ItemType.Keycard2},
+            {"Keycard3", ItemType.Keycard3},
+            {"Keycard4", ItemType.Keycard4},
+            {"Keycard5", ItemType.Keycard5},
+            {"Medkit", ItemType.Medkit},
+            {"NVGoggles", ItemType.NVGoggles},
+            {"P90", ItemType.P90},
+            {"Radio", ItemType.Radio},
+            {"SCP714", ItemType.SCP714},
+            {"SCP035", ItemType.SCP035},
+            {"SCP1025", ItemType.SCP1025},
+            {"SCP1499", ItemType.SCP1499},
+            {"SNav", ItemType.SNav}
         };
 
         public static ItemType fromID(string ID)
