85123e97b72f286352e8bfbf9e354e0b9492a345
PintTheDragon <PintTheDragon@hotmail.com>
2021-01-08 11:44:33 -0800

    Add new properties and a config manager

---
+++ ETAPI/Features/Config.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace ETAPI.Features
+{
+    public static class Config
+    {
+        /// <summary>
+        /// A function to load config information. If the config file does not exist or is invalid, it will be created.
+        /// </summary>
+        /// <param name="name">The name of the config file, excluding the type. For example, the name "SCPStats" would create a file "SCPStats.json".</param>
+        /// <typeparam name="T">The config object. This object will be serialized into JSON.</typeparam>
+        /// <returns>The deserialized JSON object.</returns>
+        public static T AddConfig<T>(string name) where T : new()
+        {
+            T config;
+            
+            var str = Path.Combine(Application.dataPath, "../settings/plugins/"+name.Replace(".", "").Replace("/", "").Replace("\\", "")+".json");
+            if (!File.Exists(str))
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(str))) Directory.CreateDirectory(Path.GetDirectoryName(str));
+                File.WriteAllText(str, JsonConvert.SerializeObject((object) new T(), Formatting.Indented));
+            }
+            try
+            {
+                config = JsonConvert.DeserializeObject<T>(File.ReadAllText(str));
+            }
+            catch (NullReferenceException)
+            {
+                File.WriteAllText(str, JsonConvert.SerializeObject((object) new T(), Formatting.Indented));
+                config = new T();
+            }
+            catch (JsonException)
+            {
+                File.Move(str, Path.Combine(Application.dataPath, "../settings/plugins/INVALID-"+name.Replace(".", "").Replace("/", "").Replace("\\", "")+".json"));
+                File.WriteAllText(str, JsonConvert.SerializeObject((object) new T(), Formatting.Indented));
+                config = new T();
+            }
+
+            return config;
+        }
+    }
+}
--- ETAPI/Features/Server.cs
+++ ETAPI/Features/Server.cs
@@ -1,23 +1,31 @@
-﻿using Assets.MirrorEXT;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.MirrorEXT;
 using Discord;
+using JetBrains.Annotations;
 using LiteMirror;
 using LiteNetLibMirror;
+using VirtualBrightPlayz.SCP_ET.Misc;
 
 namespace ETAPI.Features
 {
     public static class Server
     {
+        private static List<string> nameItems = new List<string>();
+        private static string curName = null;
+        
         /// <summary>
-        /// Gets the server's IP. This can be either an actual IP, or a lobby ID. To check if it is a lobby, use the IsLobby property.
+        /// Gets the server's IP. This can be either an actual IP, or a lobby ID. To check if it is a lobby, use the IsLobby property. If this is null, then an error has most likely occured.
         /// </summary>
+        [CanBeNull]
         public static string IP
         {
             get
             {
-                var address = "";
+                string address = null;
                 if (CustomNetworkManager.manager.server.transport is LiteNetLibTransport)
                 {
-                    address = CustomNetworkManager.manager.currentServerAddress;
+                    address = CustomNetworkManager.manager.server.transport.ServerUri().FirstOrDefault()?.Host;
                 }
                 else
                 {
@@ -26,6 +34,44 @@ namespace ETAPI.Features
 
                 return address;
             }
+        }
+
+        /// <summary>
+        /// Gets the server's port. This will be -1 if the server is a lobby.
+        /// </summary>
+        public static int Port => CustomNetworkManager.manager.server.transport.ServerUri().FirstOrDefault()?.Port ?? -1;
+
+        /// <summary>
+        /// Gets or sets the server's name that will appear in the server list. If you want to add tokens to the server name, using AddToName and RemoveFromName methods are recommended.
+        /// </summary>
+        public static string Name
+        {
+            get => GameSettings.serverSettings.serverName;
+            set => GameSettings.serverSettings.serverName = value;
+        }
+
+        /// <summary>
+        /// Adds a new item to the end of the server name. This is useful for adding tokens to the name.
+        /// </summary>
+        /// <param name="item">The item to be added.</param>
+        public static void AddToName(string item)
+        {
+            if (string.IsNullOrEmpty(curName)) curName = Name;
+            
+            nameItems.Add(item);
+            Name = curName + string.Join("", nameItems);
+        }
+
+        /// <summary>
+        /// Removes an item from the server name. This item must first be added with AddToName.
+        /// </summary>
+        /// <param name="item">The item to be removed.</param>
+        public static void RemoveFromName(string item)
+        {
+            if (string.IsNullOrEmpty(curName)) curName = Name;
+
+            nameItems.Remove(item);
+            Name = curName + string.Join("", nameItems);
         }
 
         /// <summary>
