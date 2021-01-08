ef9f939439b4701e3caeb5d703ee9456f9b759af
PintTheDragon <PintTheDragon@hotmail.com>
2021-01-08 00:17:09 -0800

    Added nuget

--- ETAPI/Properties/AssemblyInfo.cs
+++ ETAPI/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ETAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An API to help develop plugins for SCP: Escape Together.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PintTheDragon")]
 [assembly: AssemblyProduct("ETAPI")]
---
+++ README.md
@@ -0,0 +1,2 @@
+# ETAPI
+ETAPI is an API for SCP: Escape Together that makes plugin development a lot easier. The dll file for it should be placed in the dependencies folder.
